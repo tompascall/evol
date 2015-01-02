edf3b94ae26229f6cea6720aa44bc65d6795e1bc
tompascall <tompascall@gmail.com>
2015-01-02 15:24:45 +0100

    add smooth-scroll

--- js/index.js
+++ js/index.js
@@ -4,36 +4,38 @@
 // SLIDER snippet
 // Source: http://www.jssor.com/
 
- jQuery(document).ready(function ($) {
-        var options = {
-            $BulletNavigatorOptions: {
-                $Class: $JssorBulletNavigator$,
-                $ChanceToShow: 2,
-                $AutoCenter: 1
-            },
-            $AutoPlay: true,
-            $Loop: 1
-        };
-        var jssor_slider1 = new $JssorSlider$('slider1_container', options);
+jQuery(document).ready(function ($) {
+    var options = {
+        $BulletNavigatorOptions: {
+            $Class: $JssorBulletNavigator$,
+            $ChanceToShow: 2,
+            $AutoCenter: 1
+        },
+        $AutoPlay: true,
+        $Loop: 1
+    };
+    var jssor_slider1 = new $JssorSlider$('slider1_container', options);
 
-        //responsive code begin
-        //you can remove responsive code if you don't want the slider scales
-        //while window resizes
-        function ScaleSlider() {
-            var parentWidth = $('#slider1_container').parent().width();
-            if (parentWidth) {
-                jssor_slider1.$ScaleWidth(parentWidth);
-            }
-            else
-                window.setTimeout(ScaleSlider, 30);
+    //responsive code begin
+    //you can remove responsive code if you don't want the slider scales
+    //while window resizes
+    function ScaleSlider() {
+        var parentWidth = $('#slider1_container').parent().width();
+        if (parentWidth) {
+            jssor_slider1.$ScaleWidth(parentWidth);
         }
-        //Scale slider after document ready
-        ScaleSlider();
+        else
+            window.setTimeout(ScaleSlider, 30);
+    }
+    //Scale slider after document ready
+    ScaleSlider();
 
-        //Scale slider while window load/resize/orientationchange.
-        $(window).bind("load", ScaleSlider);
-        $(window).bind("resize", ScaleSlider);
-        $(window).bind("orientationchange", ScaleSlider);
-        //responsive code end
-    });
+    //Scale slider while window load/resize/orientationchange.
+    $(window).bind("load", ScaleSlider);
+    $(window).bind("resize", ScaleSlider);
+    $(window).bind("orientationchange", ScaleSlider);
+    //responsive code end
+});
+
+
 
